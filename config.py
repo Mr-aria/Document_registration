6196cf0f34789ab3ff03a2d6856434aae213b8b1
mr-aria <82929051+Mr-aria@users.noreply.github.com>
2026-07-01 17:09:16 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # توکن را در Railway تنظیم کن
+OWNER_ID = int(os.getenv("OWNER_ID", 1275490079))
+BANK_BOT_USERNAME = os.getenv("BANK_BOT_USERNAME", "CamelotBank_bot")
